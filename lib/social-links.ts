9c63232e6cb7e576d81710587382d92630753bb7
Ruslan <rus.eremian@gmail.com>
2026-05-09 12:44:37 +0200

    Update Instagram link

--- lib/social-links.ts
+++ lib/social-links.ts
@@ -1,3 +1,3 @@
-export const INSTAGRAM_URL = "https://www.instagram.com/aida_221";
+export const INSTAGRAM_URL = "https://www.instagram.com/commande.savoureuse";
 export const TELEGRAM_URL = "https://t.me/+33620234448";
 export const WHATSAPP_URL = "https://wa.me/33620234448";
